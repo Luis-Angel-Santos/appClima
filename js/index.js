f28bc5018eed831589317950ffffdbc39ed3cd18
Luis Angel Santos <santosangel748@gmail.com>
2022-02-10 12:23:25 -0600

    Obteniendo datos de la API

--- js/index.js
+++ js/index.js
@@ -1,4 +1,19 @@
 const apiKey = '4bb55e5704aec5380422e644d0b6a577';
-const baseUrl = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=';
-const form = document.querySelector()
+const form = document.querySelector('.container form');
+const input = document.querySelector(".container input");
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    const inputVal = input.value;
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+
+    fetch(url) 
+    .then(response => response.json())
+    .then(data => { 
+        console.log(data);
+    }).catch(() => { 
+        console.log('No hay ciudad'); 
+    });
+})
 
